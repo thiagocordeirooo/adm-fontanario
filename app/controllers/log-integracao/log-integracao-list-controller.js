7d8f9517bbfdfe15187273729afa880dd34f0210
Thiago Cordeiro <thiagocordeirooo@gmail.com>
2016-04-07 20:00:34 +0000

    subindo...

---
+++ app/controllers/log-integracao/log-integracao-list-controller.js
@@ -0,0 +1,19 @@
+(function(){
+    'use strict';
+
+    angular.module('app').controller('LogIntegracaoListController', LogIntegracaoListController);
+
+    LogIntegracaoListController.$inject = ['$rootScope', '$location'];
+
+    function LogIntegracaoListController($rootScope, $location){
+        var vm = this;
+        
+        activate();
+        ////////////////////////////////////
+
+        function activate(){
+            // Inicializador
+
+        }
+    }
+})();
---
+++ app/controllers/usuario/usuario-list-controller.js
@@ -0,0 +1,19 @@
+(function(){
+    'use strict';
+
+    angular.module('app').controller('UsuarioListController', UsuarioListController);
+
+    UsuarioListController.$inject = ['$rootScope', '$location'];
+
+    function UsuarioListController($rootScope, $location){
+        var vm = this;
+        
+        activate();
+        ////////////////////////////////////
+
+        function activate(){
+            // Inicializador
+
+        }
+    }
+})();
